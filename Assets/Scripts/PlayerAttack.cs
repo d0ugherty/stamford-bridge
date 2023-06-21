25b5dca993d92e800b8937d33b47000e7c74a9da
thomas <tdougherty16@gmail.com>
2023-06-21 19:22:54 -0400

    hit detection and prefabs

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -6,30 +6,73 @@ using UnityEngine.InputSystem;
 public class PlayerAttack : MonoBehaviour
 {
 
-    private float direction;
+    public GameObject projectilePrefab;
+    private Rigidbody2D playerRigidBody;
+    public float projectileSpeed = 5f;
+    public float maxDistance = 5f;
+
+    private bool isAttacking = false;
+
+    private void Start() {
+        playerRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && !isAttacking)
+        {
+            isAttacking = true;
+            playerRigidBody.isKinematic = true;
+            Vector2 projectileDirection = transform.localScale.x > 0f ? Vector2.right : Vector2.left;
+
+            GameObject projectileClone = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+            Rigidbody2D projectileRigidbody = projectileClone.GetComponent<Rigidbody2D>();
+            projectileRigidbody.velocity = projectileDirection * projectileSpeed;
+
+            Destroy(projectileClone, maxDistance / projectileSpeed);
+            StartCoroutine(ResetPlayerPhysics());
+        }
+    }
+
+    private IEnumerator ResetPlayerPhysics(){
+        yield return new WaitForSeconds(maxDistance / projectileSpeed);
+        playerRigidBody.isKinematic = false;
+        isAttacking = false;
+    }
+
+    /*private float direction;
     private bool isProjectileActive = false;
     private Rigidbody2D currentProjectile;
     public float distance;
-    public Rigidbody2D projectile;
+    public Rigidbody2D projectile; //projectile
+    public Rigidbody2D clone;
+    private Vector2 initialPosition;
+    public float maxDistance;
+    public bool IsDestroyed = false;
 
     // Update is called once per frame
-    void Update()
+    /*void Update()
     {
         direction = transform.localScale.x;
 
         if (Keyboard.current.spaceKey.wasPressedThisFrame && !isProjectileActive){
             Vector2 projectileVelocity = new Vector2(distance * direction, 0f);
-            projectile = Resources.Load<Rigidbody2D>("Prefabs/Projectile.prefab");
+            //projectile = Resources.Load<Rigidbody2D>("Prefabs/Projectile.prefab");
 
-            Rigidbody2D clone = Instantiate(projectile, transform.position, Quaternion.identity);
+            clone  = Instantiate(projectile, transform.position, Quaternion.identity);
             clone.velocity = projectileVelocity;
             isProjectileActive = true;
         }
 
-       // if (currentProjectile != null && currentProjectile.GetComponent<Projectile>().IsDestroyed) {
-         //   currentProjectile = null;
-           // isProjectileActive = false;
-        //}
+        initialPosition = transform.position;
 
-    }
+        float travel = Vector2.Distance(initialPosition, transform.position);
+
+        if (travel >= maxDistance && clone != null) {
+            IsDestroyed = true;
+            Destroy(clone.gameObject);
+        }
+    }*/
+
+
 }
