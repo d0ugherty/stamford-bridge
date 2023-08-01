d1c8d8d6079a7bb9759e322754bdcdc59ad9c535
Thomas Dougherty <105173634+d0ugherty@users.noreply.github.com>
2023-08-01 09:52:38 -0400

    Removed empty Update() method

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -6,27 +6,18 @@ using System;
 
 public class AudioManager : MonoBehaviour
 {
-
-    public Sound[] sounds;
+ public Sound[] sounds;
     // Start is called before the first frame update
     void Awake()
     {
         foreach (Sound sound in sounds) {
                 sound.source = gameObject.AddComponent<AudioSource>();
                 sound.source.clip = sound.clip;
-
                 sound.source.volume = sound.volume;
                 sound.source.pitch = sound.pitch;
-            
         }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
     public void Play(string name) {
         Sound sound = Array.Find(sounds, sound => sound.name == name);
         if (sound == null) {
